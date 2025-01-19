90d6b9156be760e99820d7b43bfd721faffbd8d3
Haim Hubara <haimhobara@gmail.com>
2025-01-19 17:42:37 +0200

    add colors for the app

--- constants/styles.js
+++ constants/styles.js
@@ -1,6 +1,20 @@
 export const GlobalStyles = {
     colors: {
-     // primary50: '#e4d9fd',
-     //We need to add colors as above
+
+      mainColor: '#4285F4',
+      mainColorLight: '#6A9EF6',
+      mainColorDark: '#3367D6',
+
+      highlightColor: '#FFCA28',
+      highlightColorLight: '#FFDD58',
+      highlightColorDark: '#F59D1A',
+
+      accentColor: '#FFA726',
+      accentColorLight: '#FFB84D',
+      accentColorDark: '#FB8C00',
+
+      backgroundColor: '#FFFFFF',
+      backgroundColorLight: '#F8F8F8',
+      backgroundColorDark: '#E0E0E0',
     },
   };
