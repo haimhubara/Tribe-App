3d17bac4673469d4e75646edefd6d43209e085a9
gal lifshiz <gal.lifshiz123@gmail.com>
2025-03-11 20:20:11 +0200

    search line finish

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useCallback } from 'react';
-import { View, StyleSheet, ScrollView, Platform, TextInput, TouchableOpacity, RefreshControl, Modal, Pressable } from 'react-native';
+import { View, StyleSheet, ScrollView, TextInput, TouchableOpacity, RefreshControl, Modal, Pressable } from 'react-native';
 import ActivityComponent from '../components/ActivityComponent';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import Ionicons from '@expo/vector-icons/Ionicons';
@@ -8,19 +8,18 @@ import { initializeApp } from "firebase/app";
 import { getFirestore, collection, getDocs } from "firebase/firestore";
 import firebaseConfig from "../util/firebaseConfig.json";
 
-const SearchScreen = ({ navigation, route }) => {
+const SearchScreen = ({ navigation }) => {
     const [search, setSearch] = useState('');
     const [activities, setActivities] = useState([]);
     const [refreshing, setRefreshing] = useState(false);
     const [sidebarVisible, setSidebarVisible] = useState(false);
     const [filters, setFilters] = useState({});
 
-
     const app = initializeApp(firebaseConfig);
     const db = getFirestore(app);
 
-    const handleActivityPress = (activityData,id) => {
-        navigation.navigate('PersonalActivityProfileScreen', { activity: activityData, myPage: 1 , id:id});
+    const handleActivityPress = (activityData, id) => {
+        navigation.navigate('PersonalActivityProfileScreen', { activity: activityData, myPage: 1, id: id });
     };
 
     const isoToDateString = (isoString) => {
@@ -29,16 +28,15 @@ const SearchScreen = ({ navigation, route }) => {
         if (isNaN(date.getTime())) return null; // אם התאריך לא תקף, החזר null
         return date.toISOString().split('T')[0]; // מחזיר YYYY-MM-DD
     };
-    
+
     const isoToTimeString = (isoString) => {
         if (!isoString) return null;
         const date = new Date(isoString);
         if (isNaN(date.getTime())) return null;
         return date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: false }); // מחזיר בפורמט 24 שעות
     };
-    
 
-    const fetchActivities = useCallback(async (filters = {}) => {
+    const fetchActivities = useCallback(async (filters = {}, searchTerm = '') => {
         setRefreshing(true);
         try {
             const querySnapshot = await getDocs(collection(db, "activities"));
@@ -46,8 +44,16 @@ const SearchScreen = ({ navigation, route }) => {
                 id: doc.id,
                 ...doc.data(),
             }));
-    
-            // סינון לפי תאריך התחלה וסיום
+
+            // סינון לפי חיפוש
+            if (searchTerm) {
+                fetchedActivities = fetchedActivities.filter(activity =>
+                    activity.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+                    activity.description.toLowerCase().includes(searchTerm.toLowerCase())
+                );
+            }
+
+            // סינון לפי פילטרים נוספים (תאריך, שעה, מין, משתתפים, קטגוריות, שפות וכו')
             if (filters.dateStart || filters.dateEnd) {
                 fetchedActivities = fetchedActivities.filter(activity => {
                     const activityDate = new Date(activity.date);
@@ -56,68 +62,46 @@ const SearchScreen = ({ navigation, route }) => {
                 });
             }
 
-            if (filters.timeStart || filters.timeEnd) {
-                fetchedActivities = fetchedActivities.filter(activity => {
-                    if (!activity.time) return false; // אם אין זמן, הפעילות לא תיכלל
-                    
-                    const activityTime = new Date(activity.time);
-                    if (isNaN(activityTime.getTime())) return false; // אם הזמן אינו תקף, דלג על הפעילות
-            
-                    return (!filters.timeStart || new Date(filters.timeStart) <= activityTime) &&
-                           (!filters.timeEnd || new Date(filters.timeEnd) >= activityTime);
-                });
-            }
-            
-    
-
             if (filters.selectedGender && filters.selectedGender !== "Any") {
                 fetchedActivities = fetchedActivities.filter(activity => activity.gender === filters.selectedGender);
             }
 
-            if(filters.selectedNumPartitions>1){
+            if (filters.selectedNumPartitions > 1) {
                 fetchedActivities = fetchedActivities.filter(activity => activity.selectedNumPartitions >= filters.selectedNumPartitions);
             }
-    
 
             if (Array.isArray(filters.selectedCategories) && filters.selectedCategories.length > 0) {
                 fetchedActivities = fetchedActivities.filter(activity =>
                     filters.selectedCategories.some(category => activity.categories.includes(category))
                 );
             }
-    
+
             if (Array.isArray(filters.selectedLanguages) && filters.selectedLanguages.length > 0) {
                 fetchedActivities = fetchedActivities.filter(activity =>
                     filters.selectedLanguages.some(language => activity.languages.includes(language))
                 );
             }
-    
+
             setActivities(fetchedActivities);
         } catch (error) {
             console.error("Error fetching activities:", error);
         }
         setRefreshing(false);
     }, [db]);
-    
-    
 
+    // קריאה ראשונית לנתונים
     useEffect(() => {
-        fetchActivities();
-    }, [fetchActivities]);
+        fetchActivities(filters, search);
+    }, [search, fetchActivities, filters]);
 
     const onRefresh = useCallback(() => {
-        fetchActivities();
-    }, [fetchActivities]);
-
-    const applyFilters = (filters) => {
-        setFilters(filters); // שמירת הפילטרים
-        fetchActivities(filters); // קריאה לפונקציה שמביאה את הנתונים עם הפילטרים
-    };
-    
+        fetchActivities(filters, search);
+    }, [fetchActivities, filters, search]);
 
     return (
         <SafeAreaView edges={['top', 'left', 'right']} style={styles.container}>
             <View style={styles.headerContainer}>
-            <Sidebar applyFilters={applyFilters} />
+                <Sidebar applyFilters={setFilters} />
                 <View style={styles.searchContainer}>
                     <Ionicons name="search" size={16} color="grey" style={styles.searchIcon} />
                     <TextInput 
@@ -134,9 +118,7 @@ const SearchScreen = ({ navigation, route }) => {
 
             <ScrollView
                 contentContainerStyle={styles.scrollContainer}
-
-                refreshControl={<RefreshControl refreshing={refreshing} onRefresh={() => fetchActivities(filters)} />}
-
+                refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}
             >
                 {activities
                     .sort((a, b) => a.distance - b.distance)
@@ -154,7 +136,6 @@ const SearchScreen = ({ navigation, route }) => {
                         </TouchableOpacity>
                     ))}
             </ScrollView>
-
 
             {/* Modal של ה-Sidebar */}
             <Modal visible={sidebarVisible} animationType="slide" transparent>
@@ -176,10 +157,6 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         alignItems: 'center',
         padding: 10,
-    },
-    menuButton: {
-        padding: 10,
-        marginRight: 10,
     },
     searchContainer: {
         flexDirection: 'row',
@@ -222,3 +199,4 @@ const styles = StyleSheet.create({
 });
 
 export default SearchScreen;
+
