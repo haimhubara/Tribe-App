37b0eb402a8bd46fd24dc961a79a37ca8604af67
Haim Hubara <haimhobara@gmail.com>
2025-04-27 17:22:09 +0300

    Add play pause button for ios

--- components/imagesAndVideo/VideoScreen.js
+++ components/imagesAndVideo/VideoScreen.js
@@ -1,12 +1,13 @@
 import { useEvent } from 'expo';
 import { useVideoPlayer, VideoView } from 'expo-video';
-import { StyleSheet, Text, useWindowDimensions, View } from 'react-native';
+import { Button, StyleSheet, Text, useWindowDimensions, View ,Platform} from 'react-native';
 
 
 
 export default function VideoScreen({videoSource, style, play}) {
 
    const { width, height } = useWindowDimensions();
+   
 
 
   const player = useVideoPlayer(videoSource, player => {
@@ -14,18 +15,36 @@ export default function VideoScreen({videoSource, style, play}) {
     play && player.play();
   });
 
+  const { isPlaying } = useEvent(player, 'playingChange', { isPlaying: player.playing });
+  
+
   return (
     <View style={[styles.contentContainer, { width:width*0.9, height:width*0.9},style]}>
-      { videoSource ?
+      { videoSource !== "" ?
+      <>
         <VideoView
-         style={styles.video} 
-          player={player}
-          allowsFullscreen
-          allowsPictureInPicture
-          VideoContentFit='cover'
-          contentFit="cover" 
-         
+            style={styles.video} 
+            player={player}
+            allowsFullscreen
+            allowsPictureInPicture
+            VideoContentFit='cover'
+            contentFit="cover" 
         />
+      {Platform.OS === 'ios' && !play &&
+        <View style={styles.controlsContainer}>
+            <Button
+              title={isPlaying ? 'Pause' : 'Play'}
+              onPress={() => {
+                if (isPlaying) {
+                  player.pause();
+                } else {
+                  player.play();
+                }
+              }}
+            />
+          </View>
+        }
+      </>
         :
         <Text>Add Video</Text>
       }
@@ -44,6 +63,9 @@ const styles = StyleSheet.create({
     height:'100%',
     borderRadius: 4,  
    
+  },
+  controlsContainer: {
+    padding: 10,
   },
 
 });
